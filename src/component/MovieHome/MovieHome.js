f7abd76d9f0b5fa0e80adb0b78d3c28eb356a4e5
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-03-03 15:17:44 +0100

    Update MovieHome.js

--- src/component/MovieHome/MovieHome.js
+++ src/component/MovieHome/MovieHome.js
@@ -11,7 +11,7 @@ const API_URL =  'http://www.omdbapi.com/?apikey=9f27771b'
 
 
 
-//     "Type": "movie",
+
 //     "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
 // }
 
